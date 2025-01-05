e61a3a6154335e3ace2cc177f13502771078d805
Aline Areda <aredaaline@gmail.com>
2025-01-05 06:31:30 -0300

    Ajuste de styles em produtos

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Head from "next/head";
-import styles from "./products.module.scss";
+import styles from "./styles.module.scss";
 import { Header } from "../../components/Header";
 import Footer from "../../components/Footer";
 import { setupAPIClient } from "../../services/api";
@@ -37,14 +37,51 @@ export default function Products() {
       </Head>
       <div>
         <Header />
-        <main className={styles.container}>
-          <h1>Produtos</h1>
-          <ul className={styles.productList}>
+        <main
+          className={styles.container}
+          id="product-container"
+          data-testid="product-container"
+        >
+          <h1
+            className={styles.title}
+            id="product-title"
+            data-testid="product-title"
+          >
+            Produtos
+          </h1>
+          <ul
+            className={styles.productList}
+            id="product-list"
+            data-testid="product-list"
+          >
             {products.map((product) => (
-              <li key={product.id} className={styles.productItem}>
-                <strong>{product.name}</strong>
-                <p>{product.description}</p>
-                <span>R$ {parseFloat(product.price).toFixed(2)}</span>
+              <li
+                key={product.id}
+                className={styles.productItem}
+                id={`product-item-${product.id}`}
+                data-testid="product-item"
+              >
+                <strong
+                  className={styles.productName}
+                  id={`product-name-${product.id}`}
+                  data-testid={`product-name-${product.id}`}
+                >
+                  {product.name}
+                </strong>
+                <p
+                  className={styles.productDescription}
+                  id={`product-description-${product.id}`}
+                  data-testid={`product-description-${product.id}`}
+                >
+                  {product.description}
+                </p>
+                <span
+                  className={styles.productPrice}
+                  id={`product-price-${product.id}`}
+                  data-testid={`product-price-${product.id}`}
+                >
+                  R$ {parseFloat(product.price).toFixed(2)}
+                </span>
               </li>
             ))}
           </ul>
